6b4e8158f68f845e9e772caffb5f1a233e9ac02b
basselashraf96 <bassel.ashraf.gharib@gmail.com>
2023-03-04 16:40:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+/***********  DOM Elements ***************/
+
+const chessPiece = document.querySelector(".block img")
+const blocks = document.querySelectorAll(".block")
+
+
+/***********  Events ***************/
+
+chessPiece.addEventListener("dragstart", dragStartHandler)
+
+blocks.forEach(item => {
+    item.addEventListener("dragover", dragOverHandler)
+    item.addEventListener("drop", dragHandler)
+    item.addEventListener("dragenter", dragEnterHandler)
+    item.addEventListener("dragleave", dragLeaveHandler)
+})
+
+
+/***********  Global Scope  **********/
+let DraggedItem
+
+/***********  Handlers ***************/
+
+function dragStartHandler(e) {
+    DraggedItem = e.target
+}
+
+function dragHandler(e) {
+    e.target.append(DraggedItem)
+    e.target.classList.remove("active")
+}
+
+function dragOverHandler(e) {
+    e.preventDefault()
+}
+
+function dragEnterHandler(e) {
+    e.target.classList.add("active")
+}
+
+function dragLeaveHandler(e) {
+    e.target.classList.remove("active")
+}
