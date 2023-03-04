f24340861374d776361ace120398bf1b3ca5c0d8
basselashraf96 <bassel.ashraf.gharib@gmail.com>
2023-03-04 17:46:45 +0200

    added touch move to the queen for mobile

--- app.js
+++ app.js
@@ -15,15 +15,29 @@ blocks.forEach(item => {
     item.addEventListener("dragleave", dragLeaveHandler)
 })
 
+/***********  Events (Mobile) *******/
+
+if (screen.width <= 765) {
+    chessPiece.addEventListener("touchstart", touchStartHandler)
+
+    blocks.forEach(item => {
+        item.addEventListener("touchmove", touchMoveHandler)
+    })
+}
 
 /***********  Global Scope  **********/
 
 let DraggedItem
 
+if (screen.width <= 765) {
+    let DraggedItemMobile
+}
+
 /***********  Handlers ***************/
 
 function dragStartHandler(e) {
     DraggedItem = e.target
+    console.log(screen.width);
 }
 
 function dragHandler(e) {
@@ -41,4 +55,18 @@ function dragEnterHandler(e) {
 
 function dragLeaveHandler(e) {
     e.target.classList.remove("active")
+}
+
+/***********  Handlers (mobile) ***************/
+
+function touchStartHandler(e) {
+    DraggedItemMobile = e.target
+    console.log(touchLocation);
+
+}
+
+function touchMoveHandler(e) {
+    let touchLocation = e.targetTouches[0];
+    chessPiece.style.left = touchLocation.pageX + 'px';
+    chessPiece.style.top = touchLocation.pageY + 'px';
 }
